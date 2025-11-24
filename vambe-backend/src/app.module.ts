cdc5ad01c206b2f0ec15921a95c24db3100c6111
Diego Sfeir <dsfeir@uc.cl>
2025-11-23 23:02:14 -0300

    🔧 deploy: edit enviroment configuration to use root env

--- vambe-backend/src/app.module.ts
+++ vambe-backend/src/app.module.ts
@@ -4,12 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CsvParserModule } from './csv-parser/csv-parser.module';
 import { AiClassificationModule } from './ai-classification/ai-classification.module';
+import { resolve } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // Hace que las variables estén disponibles globalmente
-      envFilePath: '.env', // Especifica la ruta del archivo .env
+      envFilePath: resolve(__dirname, '../../.env'), // Apunta al .env en la raíz del proyecto
     }),
     CsvParserModule,
     AiClassificationModule,
